87cf987e02efe2f26bbc21c80f1a18d73b311cae
ipanyok <ipanyok@gmail.com>
2023-04-03 12:43:03 +0300

    reformat actions by codes

--- src/main/java/ua/datastech/omnitracker/controller/OmnitrackerController.java
+++ src/main/java/ua/datastech/omnitracker/controller/OmnitrackerController.java
@@ -2,7 +2,9 @@ package ua.datastech.omnitracker.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +33,7 @@ public class OmnitrackerController {
 
     @PostMapping(value = "/api/call-dispatch-req", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public OmniTrackerResponse omnitrackerApiBlocking(@RequestBody OmniTrackerRequest request) {
-        log.info("call-dispatch-req()... objectId: " + request.getObjectID());
+        log.info("receive call-dispatch-req()... objectId: " + request.getObjectID());
         omnitrackerService.saveOmniBlockRequest(request);
         return OmniTrackerResponse.builder()
                 .externalID(request.getObjectID())
@@ -40,11 +42,17 @@ public class OmnitrackerController {
 
     @PostMapping(value = "/api/call-user-info", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public OmniTrackerResponse omnitrackerApiGetAttachmentsInfo(@RequestBody OmniTrackerAttachmentInfoRequest request) {
-        log.info("call-update-req()... objectId: " + request.getObjectID());
+        log.info("receive call-update-req()... objectId: " + request.getObjectID());
         omnitrackerService.saveOmniAttachmetRequest(request);
         return OmniTrackerResponse.builder()
                 .externalID(request.getObjectID())
                 .build();
+    ***REMOVED***
+
+    @PostMapping(value = "/api/call-closure-ack", produces = MediaType.TEXT_PLAIN_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity omnitrackerApiClosureAck(@RequestBody String request) {
+        log.info("receive call-closure-ack()");
+        return new ResponseEntity(HttpStatus.OK);
     ***REMOVED***
 
 ***REMOVED***
